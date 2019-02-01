856df6b68dadab3e59cbab64a4e81a589ca81698
Dani <daniel.anido17@gmail.com>
2019-02-01 19:33:28 +0100

    First commit

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, ActivityIndicator, StyleSheet, TouchableOpacity } from 'react-native';
+
+
+class ShareSheet extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+    render() {
+      return(
+      <View>
+        <Text>Share component</Text>
+      </View>
+    );
+  }
+}
+
+export default ShareSheet;
